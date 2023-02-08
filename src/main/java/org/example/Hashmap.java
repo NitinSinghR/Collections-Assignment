7e20acf5e62bbbb137e3babc73e9f47c31d88b86
NitinSinghR <nitin.singh@tringapps.com>
2023-02-08 11:36:35 +0530

    Add files via upload

--- src/main/java/org/example/Hashmap.java
+++ src/main/java/org/example/Hashmap.java
@@ -5,25 +5,25 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 class Hashmap {
     public static void main(String[] args){
-        HashMap<Integer,String> map=new HashMap<>();
         Logger l = Logger.getLogger("com.api.jar");
+        HashMap<Integer,String> map=new HashMap<>();
 
+        map.put(4,"Grapes");
+        map.put(2,"Orange");
         map.put(3,"Apple");
         map.put(1,"JackFruit");
-        map.put(2,"Orange");
-        map.put(4,"Grapes");
 
         l.log(Level.INFO,()->"Size: " +map.size());
-        l.log(Level.INFO,()->"Keys : " + map.keySet());
         l.log(Level.INFO,()->"Values : " + map.values());
+        l.log(Level.INFO,()->"Keys : " + map.keySet());
         l.log(Level.INFO,()-> "Key-Value pairs: " + map.entrySet());
-        map.remove(3);
-
-        l.info("After removing ");
-        l.log(Level.INFO,()-> "Key-Value pairs: " + map.entrySet());
+        map.remove(2);
 
         l.log(Level.INFO,()-> "Getting a value"+map.get(2));
         map.replace(2,"Kiwi");
+
+        l.info("After removing ");
+        l.log(Level.INFO,()-> "Key-Value pairs: " + map.entrySet());
 
         l.info("After replacing ");
         l.log(Level.INFO,()-> "Key-Value pairs: " + map.entrySet());
